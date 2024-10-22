4f26525563a41c7cf8cda2432214939be37c7024
David Llewellyn-Jones <david@flypig.co.uk>
2024-10-22 21:30:05 +0100

    Add Truth section and update CEF API
    Adds a couple of quotes for "The Truth About Browsers" and updates the
    CEF interface to include CefV8Value to explain how data can be shared
    between the native code and JavaScript code.

--- code-examples/blink-cef.cpp
+++ code-examples/blink-cef.cpp
@@ -1,25 +1,15 @@
 class CefBrowserHost : public CefBaseRefCounted {
  public:
   static bool CreateBrowser(const CefWindowInfo& windowInfo,
-                            CefRefPtr<CefClient> client,
-                            const CefString& url,
-                            const CefBrowserSettings& settings,
-                            CefRefPtr<CefDictionaryValue> extra_info,
-                            CefRefPtr<CefRequestContext> request_context);
+               CefRefPtr<CefClient> client,
+               const CefString& url,
+               const CefBrowserSettings& settings,
+               CefRefPtr<CefDictionaryValue> extra_info,
+               CefRefPtr<CefRequestContext> request_context);
   CefRefPtr<CefBrowser> GetBrowser();
   void CloseBrowser(bool force_close);
   ...
   void StartDownload(const CefString& url);
-  void PrintToPDF(const CefString& path,
-                  const CefPdfPrintSettings& settings,
-                  CefRefPtr<CefPdfPrintCallback> callback);
-  void Find(const CefString& searchText,
-                    bool forward,
-                    bool matchCase,
-                    bool findNext);
-  void StopFinding(bool clearSelection);
-  bool IsFullscreen();
-  void ExitFullscreen(bool will_cause_resize);
   ...
 };
 
@@ -36,7 +26,7 @@ class CefFrame : public CefBaseRefCounted {
                          const CefString& script_url,
                          int start_line);
   void SendProcessMessage(CefProcessId target_process,
-                          CefRefPtr<CefProcessMessage> message);
+                       CefRefPtr<CefProcessMessage> message);
   ...
 };
 
@@ -51,5 +41,21 @@ class CefBrowser : public CefBaseRefCounted {
   void StopLoad();
   bool HasDocument();
   CefRefPtr<CefFrame> GetMainFrame();
+  ...
 };
 
+class CefV8Value : public virtual CefBaseRefCounted {
+ public:
+  static CefRefPtr<CefV8Value> CreateInt(int32_t value);
+  bool IsInt();
+  int32_t GetIntValue();
+  static CefRefPtr<CefV8Value> CreateDouble(double value);
+  ...
+
+  static CefRefPtr<CefV8Value> CreateFunction(
+    const CefString& name, CefRefPtr<CefV8Handler> handler);
+  CefRefPtr<CefV8Value> ExecuteFunction(
+      CefRefPtr<CefV8Value> object,
+      const CefV8ValueList& arguments);
+  ...
+};
