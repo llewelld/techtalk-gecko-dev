0026d661f7a2961cfa9f6a7976ef81126ae001b6
David Llewellyn-Jones <david@flypig.co.uk>
2024-11-24 22:29:50 +0000

    Update dom walk code
    Updates the dom walk code to reflect changes from the actual
    implementation. The changes ensure that the code can be executed
    multiple times without causing a warning due to the same variable being
    declared multiple times.

--- code-examples/dom-walk.js
+++ code-examples/dom-walk.js
@@ -1,4 +1,4 @@
-function node_stats(global_ctx, local_ctx, node) {
+function collect_node_stats(global_ctx, local_ctx, node) {
     // Update the context
     local_ctx.depth += 1;
     local_ctx.breadth.push(node.childNodes.length);
@@ -9,9 +9,10 @@ function node_stats(global_ctx, local_ctx, node) {
     // Recurse into child nodes
     for (child of node.childNodes) {
         child_ctx = structuredClone(local_ctx);
-        child_ctx.breadth = local_ctx.breadth.
-            slice(0, local_ctx.depth + 1);
-        child_ctx = node_stats(global_ctx, child_ctx, child);
+        child_ctx.breadth = local_ctx.breadth.slice(
+            0, local_ctx.depth + 1);
+        child_ctx = collect_node_stats(
+            global_ctx, child_ctx, child);
 
         // Recalculate the child breadths
         for (let i = local_ctx.depth + 1;
@@ -23,7 +24,7 @@ function node_stats(global_ctx, local_ctx, node) {
 
     // Paint the DOM red
     if (node.style) {
-        node.style.border = "1px dashed red";
+        node.style.boxShadow = "inset 0px 0px 1px 0.5px red";
     }
 
     // Move back up the tree
@@ -31,22 +32,27 @@ function node_stats(global_ctx, local_ctx, node) {
     return local_ctx;
 }
 
-// Data available to all nodes
-global_ctx = {
-    "nodes": 0,
-    "maxdepth": 0,
-    "maxbreadth": 0
-};
+function node_stats() {
+    // Data available to all nodes
+    let global_ctx = {
+        "nodes": 0,
+        "maxdepth": 0,
+        "maxbreadth": 0
+    }
 
-// Data that's local to the node and shared with the parent
-local_ctx = {
-    "depth": 0,
-    "breadth": [1]
-};
+    // Data local to the node and shared with the parent
+    let local_ctx = {
+        "depth": 0,
+        "breadth": [1]
+    }
 
-// Off we go
-local_ctx = node_stats(global_ctx, local_ctx, document);
-global_ctx.maxbreadth = Math.max.apply(null, local_ctx.breadth);
+    // Off we go
+    local_ctx = collect_node_stats(
+        global_ctx, local_ctx, document);
+    global_ctx.maxbreadth = Math.max.apply(
+        null, local_ctx.breadth);
+    return global_ctx;
+}
 
 // Return the results (only strings allowed)
-return JSON.stringify(global_ctx);
+JSON.stringify(node_stats())
